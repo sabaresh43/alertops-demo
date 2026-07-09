2fd331545e7c52d5aed44b18dec48e63337d94c0
sabaresh <sabaresh43@gmail.com>
2026-07-09 17:01:38 +0530

    pipeline fixed

--- demo_app/pricing.py
+++ demo_app/pricing.py
@@ -1,5 +1,3 @@
 def apply_percentage_discount(price: float, percentage: float) -> float:
     """Return the price after applying a percentage discount."""
-    # Intentional demo defect: the discount is added instead of subtracted.
-    return round(price + (price * percentage / 100), 2)
-
+    return round(price - (price * percentage / 100), 2)
