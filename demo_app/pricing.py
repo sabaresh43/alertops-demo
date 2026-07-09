4069fc9be3ae7e4297bd404cf0aa902c0ed4ad4f
sabaresh <sabaresh43@gmail.com>
2026-07-09 21:51:11 +0530

    bugprce

--- demo_app/pricing.py
+++ demo_app/pricing.py
@@ -1,3 +1,3 @@
 def apply_percentage_discount(price: float, percentage: float) -> float:
     """Return the price after applying a percentage discount."""
-    return round(price - (price * percentage / 100), 2)
+    return round(price + (price * percentage / 100), 2)
